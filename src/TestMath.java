6dac5185a240ebfc56d5cdf88e8ed2fb24f394f6
vidyar <vidyar@qhode.com>
2013-10-10 16:32:27 +0530

    Update TestMath.java

--- src/TestMath.java
+++ src/TestMath.java
@@ -23,7 +23,7 @@ public class TestMath extends TestCase {
   public void testsubtract() {
 	  	int num1 = 5;
 	  	int num2 = 2;
-	  	int total = 1;
+	  	int total = 3;
 	  	int sum = 0;
 	  	sum = Math.subtract(num1, num2);
 	  	assertEquals(sum, total);
