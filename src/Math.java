c56eda97b4fbace814b06cc61a60e9df6751cc48
Vidya Raghavan <vidya@shippable.com>
2014-02-22 17:16:52 +0530

    Update Math.java

--- src/Math.java
+++ src/Math.java
@@ -2,15 +2,15 @@ public class Math {
 
 	static public int add(int a, int b) {
 	
-		return a * b;
+		return a + b;
 	}
 	
 	static public int multiply ( int a, int b) {
 	
-		return a + b;
+		return a * b;
 	}
 	public static int subtract(int a, int b) {
 		
-		return a + b;
+		return a - b;
 		}
 }
